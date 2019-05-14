ac3a2736db97cd494c384bde5c687affb3920f05
Jake White <jwhite52@gmail.com>
2019-05-14 18:28:01 -0400

    Create model

--- majormanager/golf/models.py
+++ majormanager/golf/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class League(models.Model):
+  league_name = models.CharField(max_length=200)
+
+  def __str__(self):
+    return self.league_name
+
+class Players(models.Model):
+  player_name = models.CharField(max_length=200)
+  score = models.IntegerField(null=True)
+
+  def __str__(self):
+    return self.player_name
+
+class Team(models.Model):
+  league = models.ForeignKey(League, on_delete=models.CASCADE)
+  players = models.ManyToManyField(Players)
+  team_name = models.CharField(max_length=200)
+
+  def __str__(self):
+    return self.team_name
