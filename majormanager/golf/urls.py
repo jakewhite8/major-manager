2f3f02652f91ee138050fd36685840bac722e80b
Jake White <jwhite52@gmail.com>
2019-05-21 16:52:21 -0400

    Add player detail page
    Add page that displays a players name and current score

--- majormanager/golf/urls.py
+++ majormanager/golf/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'golf'
 urlpatterns = [
   path('', views.IndexView.as_view(), name="index"),
+  path('player/<int:pk>/', views.PlayerView.as_view(), name="player"),
 ]
--- majormanager/golf/views.py
+++ majormanager/golf/views.py
@@ -1,4 +1,8 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Players, League, Team
 
@@ -6,6 +10,13 @@ from .models import Players, League, Team
 class IndexView(generic.ListView):
   template_name = 'golf/index.html'
   context_object_name = 'players_list'
+
+  def get_queryset(self):
+    return Players.objects.all()
+
+class PlayerView(generic.DetailView):
+  model = Players
+  template_name = 'golf/update.html'
 
   def get_queryset(self):
     return Players.objects.all()
